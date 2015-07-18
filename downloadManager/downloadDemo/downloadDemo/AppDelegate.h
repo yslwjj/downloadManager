a1b81d3ba1dde5d545108bbe1aac742236fca633
sam <samyang1107@icloud.com>
2015-07-18 16:05:10 +0800

    add project

---
+++ downloadManager/downloadDemo/downloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  downloadDemo
+//
+//  Created by 杨森 on 15/7/17.
+//  Copyright (c) 2015年 samyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ downloadManager/downloadDemo/downloadDemo/SAMSingleton.h
@@ -0,0 +1,29 @@
+
+#define  SAMSingletonH(name)  + (instancetype)shared##name;
+
+#define  SAMSingletonM(name)  \
+\
+static id _instance; \
+\
++ (instancetype)allocWithZone:(struct _NSZone *)zone \
+{ \
+    static dispatch_once_t onceToken; \
+    dispatch_once(&onceToken, ^{  \
+        _instance = [super allocWithZone:zone]; \
+    }); \
+    return _instance; \
+} \
+  \
++ (instancetype)shared##name  \
+{  \
+    static dispatch_once_t onceToken; \
+    dispatch_once(&onceToken, ^{  \
+        _instance = [[self alloc] init]; \
+    }); \
+    return _instance; \
+} \
+ \
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+    return _instance; \
+}
